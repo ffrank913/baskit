ef5a46e6228aae4279217c3941566d0e646954c7
Felix Frank <felix.frank@twt.de>
2023-11-29 14:09:52 +0100

    Added Clear button to basket list.

--- src/router/routes/Basket.tsx
+++ src/router/routes/Basket.tsx
@@ -7,11 +7,12 @@ import { useBasketItemContext } from "../../context/basketItems/BasketItemsConte
 import { AssetLib } from "../../AssetLib";
 import { useState } from "react";
 import AddCustomItem from "../../components/basket/AddCustomItem";
+import { BlurView } from "expo-blur";
 
 export default function Basket() {
   const { addItem, clearAllItems } = useBasketItemContext();
 
-  const [showClearModal, setShowClearModal] = useState<boolean>(false);
+  const [showClearModal, setShowClearModal] = useState<boolean>(true);
 
   return (
     <View style={styles.container}>
@@ -19,33 +20,70 @@ export default function Basket() {
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Baskit</Text>
           <TouchableHighlight
-              style={{
-                width: 48,
-                height: 48,
-                marginLeft: 16,
-                zIndex: 1,
-                borderRadius: 24,
-              }}
-              onPress={async () => {
-                setShowClearModal(true);
-              }}
-            >
-              <Image
-                style={{ left: "12%", top: "12%", width: "75%", height: "75%" }}
-                source={AssetLib.Trash}
-              ></Image>
-            </TouchableHighlight>
+            style={{
+              width: 48,
+              height: 48,
+              marginLeft: 16,
+              zIndex: 1,
+              borderRadius: 8,
+            }}
+            underlayColor={"rgba(138, 73, 95, 1.0)"}
+            onPress={async () => {
+              setShowClearModal(true);
+            }}
+          >
+            <Image
+              style={{ left: "12%", top: "12%", width: "75%", height: "75%" }}
+              source={AssetLib.Trash}
+            ></Image>
+          </TouchableHighlight>
         </View>
         <BasketList></BasketList>
-        <View style={{height: 44, backgroundColor: '#eb6b96', flexDirection: "row", bottom: 0 }}>
-          <AddCustomItem confirmData={(ingredient: IIngredient) => {
-            addItem(ingredient);
-          }}></AddCustomItem>
+        <View
+          style={{
+            height: 44,
+            backgroundColor: "#eb6b96",
+            flexDirection: "row",
+            bottom: 0,
+          }}
+        >
+          <AddCustomItem
+            confirmData={(ingredient: IIngredient) => {
+              addItem(ingredient);
+            }}
+          ></AddCustomItem>
         </View>
       </View>
       {showClearModal && (
         <View style={styles.clearContainer}>
-
+          <BlurView style={styles.clearModal} intensity={10}>
+            <Text style={styles.clearModalTitle}>Alle Elemente löschen?</Text>
+            <Text style={styles.clearModalText}>
+              Bist du Dir sicher, dass du alle Elemente in deinem Baskit löschen
+              möchtest?
+            </Text>
+            <View style={styles.clearModalButtonRow}>
+              <TouchableHighlight
+                style={styles.clearModalButtonKeep}
+                underlayColor={"rgb(54, 201, 54)"}
+                onPress={async () => {
+                  setShowClearModal(false);
+                }}
+                >
+                <Text style={styles.clearModalButtonText}>Behalten</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                style={styles.clearModalButtonDelete}
+                underlayColor={"rgb(201, 54, 54)"}
+                onPress={async () => {
+                  clearAllItems();
+                  setShowClearModal(false);
+                }}
+              >
+                <Text style={styles.clearModalButtonText}>Löschen</Text>
+              </TouchableHighlight>
+            </View>
+          </BlurView>
         </View>
       )}
     </View>
@@ -60,7 +98,48 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   clearContainer: {
-    
+    flex: 1,
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+  },
+  clearModal: {
+    padding: 16,
+    margin: 8,
+    borderRadius: 8,
+    backgroundColor: "rgba(138, 73, 95, 0.7)",
+  },
+  clearModalTitle: {
+    fontSize: 24,
+    color: "rgb(0,0,0)",
+  },
+  clearModalText: {
+    padding: 8,
+    color: "rgb(0,0,0)",
+    fontSize: 16,
+  },
+  clearModalButtonRow: {
+    flexDirection: "row",
+  },
+  clearModalButtonKeep: {
+    flex: 0.5,
+    padding: 8,
+    margin: 4,
+    alignItems: "center",
+    borderRadius: 4,
+    backgroundColor: "rgb(60, 220, 60)",
+  },
+  clearModalButtonDelete: {
+    flex: 0.5,
+    padding: 8,
+    margin: 4,
+    alignItems: "center",
+    borderRadius: 4,
+    backgroundColor: "rgb(250, 60, 60)",
+  },
+  clearModalButtonText: {
+    fontSize: 18,
   },
   titleContainer: {
     padding: 16,
@@ -77,5 +156,5 @@ const styles = StyleSheet.create({
   },
   addButtonTextStyle: {
     color: "white",
-  }
+  },
 });
